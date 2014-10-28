7c9c23b77a78d43676b26c86b618845c34ccb202
Marcin Bunsch <marcin@futuresimple.com>
2014-10-28 20:17:45 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.1.6'
+gem 'sqlite3'
+
+gem 'dropbox-api'
+gem 'choices'
+
+group :development, :test do
+  gem 'rspec-rails', '~> 3.0.0'
+end
+
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  get "/dropbox",  :controller => "dropbox", :action => "index"
+  get "/dropbox/auth",  :controller => "dropbox", :action => "show"
+  get "/dropbox/callback",  :controller => "dropbox", :action => "create"
+
+  root 'dashboard#show'
+end
